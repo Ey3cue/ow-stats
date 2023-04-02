022b72e7b2c2e91fd02f119b48786035d53e8d13
Ey3cue <cdcordisco@outlook.com>
2023-04-02 14:05:24 +0000

    refactor: rename MapType to Mode

--- db.sql
+++ db.sql
@@ -12,64 +12,64 @@ INSERT OR IGNORE INTO Queues (Queue) VALUES ('Competitive: Open Queue');
 INSERT OR IGNORE INTO Queues (Queue) VALUES ('Competitive: Role Queue');
 
 
-CREATE TABLE IF NOT EXISTS MapTypes(
-  MapType TEXT PRIMARY KEY
+CREATE TABLE IF NOT EXISTS Modes(
+  Mode TEXT PRIMARY KEY
 );
-INSERT OR IGNORE INTO MapTypes (MapType) VALUES ('Unknown');
-INSERT OR IGNORE INTO MapTypes (MapType) VALUES ('Escort');
-INSERT OR IGNORE INTO MapTypes (MapType) VALUES ('Assault');
-INSERT OR IGNORE INTO MapTypes (MapType) VALUES ('Capture the Flag');
-INSERT OR IGNORE INTO MapTypes (MapType) VALUES ('Control');
-INSERT OR IGNORE INTO MapTypes (MapType) VALUES ('Deathmatch');
-INSERT OR IGNORE INTO MapTypes (MapType) VALUES ('Elimination');
-INSERT OR IGNORE INTO MapTypes (MapType) VALUES ('Hybrid');
-INSERT OR IGNORE INTO MapTypes (MapType) VALUES ('Push');
+INSERT OR IGNORE INTO Modes (Mode) VALUES ('Unknown');
+INSERT OR IGNORE INTO Modes (Mode) VALUES ('Escort');
+INSERT OR IGNORE INTO Modes (Mode) VALUES ('Assault');
+INSERT OR IGNORE INTO Modes (Mode) VALUES ('Capture the Flag');
+INSERT OR IGNORE INTO Modes (Mode) VALUES ('Control');
+INSERT OR IGNORE INTO Modes (Mode) VALUES ('Deathmatch');
+INSERT OR IGNORE INTO Modes (Mode) VALUES ('Elimination');
+INSERT OR IGNORE INTO Modes (Mode) VALUES ('Hybrid');
+INSERT OR IGNORE INTO Modes (Mode) VALUES ('Push');
 
 CREATE TABLE IF NOT EXISTS Maps(
   MapName TEXT PRIMARY KEY,
-  MapType INTEGER,
+  Mode INTEGER,
 
-  FOREIGN KEY (MapType) REFERENCES MapTypes(MapType)
+  FOREIGN KEY (Mode) REFERENCES Modes(Mode)
 );
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Unknown', 'Unknown');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Antarctic Peninsula', 'Control');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Ayutthaya', 'Capture the Flag');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Black Forest', 'Elimination');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Blizzard World', 'Hybrid');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Busan', 'Control');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Castillo', 'Elimination');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Chateau Guillard', 'Deathmatch');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Circuit Royal', 'Escort');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Colosseo', 'Push');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Dorado', 'Escort');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Ecopoint: Antarctica', 'Elimination');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Eichenwalde', 'Hybrid');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Esperanca', 'Push');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Hanamura', 'Assault');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Havana', 'Escort');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Hollywood', 'Hybrid');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Horizon Lunar Colony', 'Assault');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Ilios', 'Control');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Junkertown', 'Escort');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Kanezaka', 'Deathmatch');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('King''s Row', 'Hybrid');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Lijiang Tower', 'Control');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Malevento', 'Deathmatch');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Midtown', 'Hybrid');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Necropolis', 'Elimination');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Nepal', 'Control');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('New Queen Street', 'Push');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Numbani', 'Hybrid');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Oasis', 'Control');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Paraiso', 'Hybrid');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Paris', 'Assault');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Petra', 'Deathmatch');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Rialto', 'Escort');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Route 66', 'Escort');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Shambali', 'Escort');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Temple of Anubis', 'Assault');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Volskaya Industries', 'Assault');
-INSERT OR IGNORE INTO Maps (MapName, MapType) VALUES ('Watchpoint: Gibraltar', 'Escort');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Unknown', 'Unknown');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Antarctic Peninsula', 'Control');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Ayutthaya', 'Capture the Flag');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Black Forest', 'Elimination');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Blizzard World', 'Hybrid');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Busan', 'Control');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Castillo', 'Elimination');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Chateau Guillard', 'Deathmatch');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Circuit Royal', 'Escort');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Colosseo', 'Push');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Dorado', 'Escort');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Ecopoint: Antarctica', 'Elimination');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Eichenwalde', 'Hybrid');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Esperanca', 'Push');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Hanamura', 'Assault');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Havana', 'Escort');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Hollywood', 'Hybrid');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Horizon Lunar Colony', 'Assault');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Ilios', 'Control');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Junkertown', 'Escort');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Kanezaka', 'Deathmatch');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('King''s Row', 'Hybrid');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Lijiang Tower', 'Control');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Malevento', 'Deathmatch');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Midtown', 'Hybrid');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Necropolis', 'Elimination');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Nepal', 'Control');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('New Queen Street', 'Push');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Numbani', 'Hybrid');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Oasis', 'Control');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Paraiso', 'Hybrid');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Paris', 'Assault');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Petra', 'Deathmatch');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Rialto', 'Escort');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Route 66', 'Escort');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Shambali', 'Escort');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Temple of Anubis', 'Assault');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Volskaya Industries', 'Assault');
+INSERT OR IGNORE INTO Maps (MapName, Mode) VALUES ('Watchpoint: Gibraltar', 'Escort');
 
 CREATE TABLE IF NOT EXISTS Players(
   Player TEXT PRIMARY KEY
